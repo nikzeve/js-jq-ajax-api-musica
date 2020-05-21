26a4b96fb7adbfa4d29fa014ecc74f229f12ca21
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-05-21 16:08:08 +0200

    push intermedia

---
+++ main.js
@@ -0,0 +1,4 @@
+// Con una chiamata ajax, recuperare i dischi musicali restituiti dall'api:
+// https://flynn.boolean.careers/exercises/api/array/music
+// Ciclare quindi i dischi e ottenuti e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
+// BONUS: creare una select con i generi dei dischi musicali (pop, rock, metal, jazz), tramite la quale si possono filtrare i dischi visualizzati (ad esempio: se nella tendina si seleziona il genere "metal", nella pagina saranno mostrati solo i dischi con il genere "metal").
