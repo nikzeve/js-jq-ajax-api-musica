dcf676349ec4a2a029af9c5e58770b65c3bb4b68
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-05-21 16:22:46 +0200

    push intermedia

--- main.js
+++ main.js
@@ -2,3 +2,30 @@
 // https://flynn.boolean.careers/exercises/api/array/music
 // Ciclare quindi i dischi e ottenuti e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
 // BONUS: creare una select con i generi dei dischi musicali (pop, rock, metal, jazz), tramite la quale si possono filtrare i dischi visualizzati (ad esempio: se nella tendina si seleziona il genere "metal", nella pagina saranno mostrati solo i dischi con il genere "metal").
+
+
+$(document).ready(function() {
+
+    var dischi_musicali = [];
+
+    $.ajax({
+        url: 'https://flynn.boolean.careers/exercises/api/array/music',
+        method: 'GET',
+        success: function(data) {
+
+            dischi_musicali = data.response;
+            console.log(dischi_musicali);
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+
+});
